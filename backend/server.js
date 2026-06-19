1271718fd3f5b23c8237d0547de45fa2f59a3ac8
musyafaapp <musyafaapp@gmail.com>
2026-06-19 21:41:53 +0700

    Fix Express v5 wildcard route bug

--- backend/server.js
+++ backend/server.js
@@ -121,7 +121,7 @@ app.post('/api/accounts', async (req, res) => {
 // Serve Frontend (Monolith concept for deployment)
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
